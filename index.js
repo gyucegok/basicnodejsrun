638f1aebff461d7b2bff0884abb0b93058e88280
Gokhan Yucegok <gyucegok@gmail.com>
2023-04-15 21:09:15 +0000

    v0.15

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  const name = process.env.NAME || 'World';
-  res.send(`Hello ${name}!`);
+  const name = process.env.NAME || 'World!';
+  res.send(`Hello ${name}`);
 });
 
 const port = parseInt(process.env.PORT) || 8080;
